b284525e6583a5a81daf3f791748db6e3f3ebbcc
Grandhys Setyo Utomo <grandhys.setyo@rekadia.co.id>
2022-04-27 22:19:08 +0700

    change bg color admin

--- resources/views/layouts/adminlayout.blade.php
+++ resources/views/layouts/adminlayout.blade.php
@@ -180,7 +180,7 @@
                 </div>
             </nav>
             <!-- Header -->
-            <div class="relative bg-second md:pt-32 pb-32 pt-12">
+            <div class="relative bg-first md:pt-32 pb-32 pt-12">
                 <div class="px-4 md:px-10 mx-auto w-full">
                     <div>
                         <!-- Card stats -->
